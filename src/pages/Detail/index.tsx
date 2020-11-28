ce0d1623e9ec54c3b91a868957a34bb8a12cc390
Gabriel Felipe Souza Santos <gabrielfelipesouzasantos@gmail.com>
2020-11-27 22:10:44 -0300

    Finished according to classes

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -16,6 +16,7 @@ interface Data {
   point: {
     name: string;
     image: string;
+    image_url: string;
     email: string;
     whatsapp: string;
     city: string;
@@ -67,7 +68,7 @@ const Detail = () => {
           <Icon name="arrow-left" size={20} color="#34CB79" />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
 
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>{data.items.map(item => item.title).join(", ")}</Text>
